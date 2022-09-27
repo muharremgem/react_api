1046ffe3fc38e1e7a42a6b32c97af77a47a65f6f
muharrem gem <muharremgem28@gmail.com>
2022-09-27 21:29:13 +0300

    clear

--- src/App.js
+++ src/App.js
@@ -1,5 +1,51 @@
+import axios from "axios";
+import React, { useState } from "react";
+
 function App() {
-  return <div className="App">selam</div>;
+  const [quote, setQuote] = useState("");
+  const [hidden, setHidden] = useState(false);
+
+  const getQuote = () => {
+    axios
+      .get("https://api.quotable.io/random")
+      .then((res) => {
+        console.log("Tıklandı");
+        setQuote(res.data.content);
+      })
+      .catch((err) => console.log(err));
+  };
+
+  return (
+    <div className="flex flex-col h-screen w-screen justify-center items-center">
+      <div
+        onClick={() => getQuote()}
+        className="p-2 bg-slate-200 hover:shadow-lg rounded-lg cursor-pointer"
+      >
+        <button>Get Random Quote</button>
+      </div>
+      {quote && (
+        <div
+          className={`mt-4 px-10 py-4 bg-gray-200 rounded-lg shadow-xl ${
+            hidden ? "hidden" : "block"
+          } `}
+        >
+          {quote}
+        </div>
+      )}
+      <button
+        onClick={() => setQuote("")}
+        className="bg-red-200 p-2 rounded-lg mt-10"
+      >
+        Clear
+      </button>
+      <button
+        className="bg-red-200 p-2 rounded-lg mt-10"
+        onClick={() => setHidden(!hidden)}
+      >
+        {hidden ? "Göster" : "Gizle"}
+      </button>
+    </div>
+  );
 }
 
 export default App;
